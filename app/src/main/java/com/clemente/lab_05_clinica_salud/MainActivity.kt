7f77615676ca3f3de789316bfd99be819a9d871d
nilton-clemente123 <nilton.clemente@tecsup.edu.pe>
2026-09-22 23:35:32 -0500

    Crear estructura inicial de Clinica Salud

---
+++ app/src/main/java/com/clemente/lab_05_clinica_salud/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.clemente.lab_05_clinica_salud
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.clemente.lab_05_clinica_salud.ui.theme.Lab_05_clinica_saludTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab_05_clinica_saludTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab_05_clinica_saludTheme {
+        Greeting("Android")
+    }
+}
