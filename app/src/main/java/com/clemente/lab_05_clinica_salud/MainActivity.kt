7656da2ffe8e8dbb0f267cbeff3846e0b617ac21
nilton-clemente123 <nilton.clemente@tecsup.edu.pe>
2026-09-22 23:48:05 -0500

    implementar pantalla basica de inicio

--- app/src/main/java/com/clemente/lab_05_clinica_salud/MainActivity.kt
+++ app/src/main/java/com/clemente/lab_05_clinica_salud/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.clemente.lab_05_clinica_salud.Screens.InicioScreen
 import com.clemente.lab_05_clinica_salud.ui.theme.Lab_05_clinica_saludTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab_05_clinica_saludTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    InicioScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab_05_clinica_saludTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/clemente/lab_05_clinica_salud/Screens/InicioScreen.kt
@@ -0,0 +1,40 @@
+package com.clemente.lab_05_clinica_salud.Screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InicioScreen(){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text("Clinica Salud+")}
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+
+            Text(
+                text = "Especialidades"
+            )
+
+            Text(
+                text = "Médicos disponibles"
+            )
+        }
+    }
+}
